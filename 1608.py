799e54a6306e08a5e06b868b53d58414744e32c4
KrinkovD <71076513+KrinkovD@users.noreply.github.com>
2021-01-26 21:04:17 +0100

    Add files via upload

--- 1608.py
+++ 1608.py
@@ -1,19 +1,19 @@
 from employee import Employee
 
 class ManageEmployee:
-	def __init__(self):
-		self.employeeList = []
-		self.beolvas()
+	def __init__(self): #konstruktor kötelező paramétere a self
+		self.employeeList = [] 	#létrehozok egy listát a konstruktorban
+		self.beolvas() #itt hívom meg a metódusokat
 		#self.szazezer()
 		self.nagy()
 	
-	def beolvas(self):
-		file = open("dolgozok.txt", "r", encoding="utf-8")
-		row = file.readline()
-		while(row):
-			rowSplit = row.split(";")
-			emp = Employee()
-			emp.number = rowSplit[0]
+	def beolvas(self):	#beolvasás + self paramétere értéke minden metódusnak
+		file = open("dolgozok.txt", "r", encoding="utf-8") #eltárolom egy file nevű változóban a beolvasott txt-t
+		row = file.readline() #címsort kiolvasom és/vagy változóba teszem az első beolvasott sort
+		while(row):	#ez addig fut, amíg a "row"-ban van bármi
+			rowSplit = row.split(";") #indexelhető elemekre osztom a sort a .split paranccsal és megadom az elválasztót
+			emp = Employee() #Az employe osztályt példányosítom egy változóban
+			emp.number = rowSplit[0]#megadom az Employee, tehát az impoortált osztály változóinak értékét
 			emp.name = rowSplit[1]
 			emp.mother_name = rowSplit[2]
 			emp.city = rowSplit[3]
@@ -23,11 +23,11 @@ class ManageEmployee:
 			emp.hiredate = rowSplit[7]
 			emp.borndate = rowSplit[8]
 			
-			self.employeeList.append(emp)
-			row = file.readline()
-		file.close()
+			self.employeeList.append(emp)	#hozzáadom az osztályban létrehozott listához az objektumot
+			row = file.readline()	#beolvasom a következő sort
+		file.close()	#ciklus lefut, fájl bezár
 			
-	def szazezer(self):
+	def szazezer(self): #MINDEN METÓDUSNAK KÖTLEZŐ ÉRTÉKE A SELF
 		bonus = []
 		for employee in self.employeeList:
 			if int(employee.bonus) < 5000:
